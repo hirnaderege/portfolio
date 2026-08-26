0d758d79635dbfd1ca55105e17e7687d67a1aa6c
Hirna Derege <168405714+hirnahate@users.noreply.github.com>
2026-08-26 12:18:50 -0700

    visualizer reroute

--- src/ProjectsPage.jsx
+++ src/ProjectsPage.jsx
@@ -29,7 +29,7 @@ export default function ProjectsPage() {
             image="images/subset/823122da-287c-4ed8-b6db-07a7bda064c3.jpeg"
             alt="GPU allocator visualizer"
             text="GPU Allocator Visualizer ✨"
-            href="/public/visualizer/main.html"
+            href="https://hirnaderege.github.io/portfolio/visualizer/main.html"
           />
       </div>
     </>
